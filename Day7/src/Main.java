e646dd3c34011e8dbb0ec8041e3520a22d0f65df
aintsuberun <kartashov414@gmail.com>
2026-04-30 14:05:02 +0300

    DAY 8

--- Day7/src/Main.java
+++ Day7/src/Main.java
@@ -1,83 +1,131 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Menu mainMenu = new Menu();
         Library lib = new Library();
-        mainMenu.showMenu();
-        int choice = scanner.nextInt();
-        scanner.nextLine();
-        while (choice != 0) {
-            if(choice == 1) {
-                do {
-                    System.out.println("Enter title name:");
-                    String title = scanner.nextLine();
-
-                    System.out.println("Enter author name:");
-                    String author = scanner.nextLine();
-
-                    System.out.println("Enter number of pages:");
-                    int pages = scanner.nextInt();
-                    scanner.nextLine();
-                    while (pages < 0) {
-                        System.out.println("Pages cannot be negative. Enter again:");
-                        pages = scanner.nextInt();
-                        scanner.nextLine();
-                    }
-
-                    System.out.println("Enter price:");
-                    double price = scanner.nextDouble();
-                    scanner.nextLine();
-                    while (price < 0) {
-                        System.out.println("Pages cannot be negative. Enter again:");
-                        price = scanner.nextDouble();
-                        scanner.nextLine();
-                    }
-
-                    Book book = new Book(title, author, pages, price);
-                    lib.addBook(book);
-                    lib.showAddedBook(book);
-
-                    System.out.println("----------------------------------\n"+
-                            "Do you want to continue?\nYes - 1\nBack to menu - 2" +
-                            "\n----------------------------------");
-                    int continueChoice = scanner.nextInt();
-                    scanner.nextLine();
-
-                    if (continueChoice != 1) {
-                        break;
-                    }
-                } while (true);
-            }
-            else if (choice == 2){
-                lib.showAllBooks();
-            }
-            else if (choice == 3){
-                System.out.println("Enter title: ");
-                String findTitle = scanner.nextLine();
-                lib.findBookByTitle(findTitle);
-            }
-            else if (choice == 4){
-                System.out.println("Enter ID: ");
-                int findId = scanner.nextInt();
-                scanner.nextLine();
-                lib.removeBookById(findId);
-            }
-            else if (choice == 5){
-                System.out.println("Enter price: ");
-                double expensivePrice = scanner.nextDouble();
-                scanner.nextLine();
-                lib.showExpensiveBooks(expensivePrice);
-            }
-            else{
-                System.out.println("Invalid value. Try again: ");
-            }
+        MenuChoice command;
+        do {
             mainMenu.showMenu();
-            choice = scanner.nextInt();
+            int choice = scanner.nextInt();
             scanner.nextLine();
-        }
+
+            command = MenuChoice.fromInt(choice);
+
+            if (command == null) {
+                System.out.println("Invalid value. Try again!");
+                continue;
+            }
+
+            switch (command) {
+                case ADD_BOOK:
+                    addBookLogic(scanner, lib);
+                    break;
+
+                case SHOW_BOOKS:
+                    lib.showAllBooks();
+                    break;
+
+                case FIND_BY_ID:
+                    System.out.println("Enter ID: ");
+                    int findById = scanner.nextInt();
+                    scanner.nextLine();
+                    lib.findById(findById);
+                    break;
+
+                case REMOVE_BOOK:
+                    System.out.println("Enter ID: ");
+                    int findId = scanner.nextInt();
+                    scanner.nextLine();
+                    lib.removeBookById(findId);
+                    break;
+
+                case SHOW_EXPENSIVE:
+                    System.out.println("Enter price: ");
+                    double expensivePrice = scanner.nextDouble();
+                    scanner.nextLine();
+                    lib.showExpensiveBooks(expensivePrice);
+                    break;
+                case FIND_BY_TITLE:
+                    System.out.println("Enter title: ");
+                    String findTitle = scanner.nextLine();
+                    lib.findBookByTitle(findTitle);
+                    break;
+
+                case SORT_BOOKS:
+                    lib.sortByPrice();
+                    lib.showAllBooks();
+                    break;
+
+                case SHOW_STATISTIC:
+                    lib.showStatistic();
+                    break;
+
+                case FIND_BY_AUTHOR:
+                    System.out.println("Enter author: ");
+                    String findAuthor = scanner.nextLine();
+                    lib.findByAuthor(findAuthor);
+                    break;
+
+                case EXIT:
+                    System.out.println("Goodbye!");
+                    break;
+            }
+        } while (command != MenuChoice.EXIT);
+        scanner.close();
+    }
+
+    private static void addBookLogic(Scanner scanner, Library lib) {
+        do {
+            System.out.println("Enter title name without spaces:");
+            String title = scanner.nextLine();
+            while(title.isBlank() || title.contains(" ")){
+                System.out.println("Title can not be empty and can not contain spaces");
+                title = scanner.nextLine();
+            }
+
+            System.out.println("Enter author name:");
+            String author = scanner.nextLine();
+            while(author.isEmpty()){
+                System.out.println("Author can not be empty");
+                System.out.println("Enter author name:");
+                author = scanner.nextLine();
+            }
+
+            System.out.println("Enter number of pages:");
+            int pages = scanner.nextInt();
+            scanner.nextLine();
+            while (pages < 0) {
+                System.out.println("Pages cannot be negative. Enter again:");
+                pages = scanner.nextInt();
+                scanner.nextLine();
+            }
+
+            System.out.println("Enter price:");
+            double price = scanner.nextDouble();
+            scanner.nextLine();
+            while (price <= 0) {
+                System.out.println("Price cannot be negative. Enter again:");
+                price = scanner.nextDouble();
+                scanner.nextLine();
+            }
+
+            Book book = new Book(title, author, pages, price);
+            lib.addBook(book);
+            lib.showAddedBook(book);
+
+            System.out.println("----------------------------------\n" +
+                    "Do you want to continue?\nYes - 1\nBack to menu - 2" +
+                    "\n----------------------------------");
+            int continueChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (continueChoice != 1) {
+                break;
+            }
+        } while (true);
     }
 }
 
@@ -125,6 +173,8 @@ class Book {
         return title;
     }
 
+    public String getAuthor(){ return author;}
+
     public int getId(){
         return id;
     }
@@ -132,24 +182,24 @@ class Book {
     public double getPrice() {
         return price;
     }
+
 }
 
-class Library{
+class Library {
     private ArrayList<Book> books;
 
-    Library(){
+    Library() {
         books = new ArrayList<>();
     }
 
-    void addBook(Book book){
+    void addBook(Book book) {
         books.add(book);
     }
 
-    void showAllBooks(){
-        if(books.isEmpty()){
+    void showAllBooks() {
+        if (books.isEmpty()) {
             System.out.println("No books in the library yet");
-        }
-        else {
+        } else {
             System.out.println("All books: ");
             for (Book book : books) {
                 book.showInfo();
@@ -157,19 +207,13 @@ class Library{
         }
     }
 
-
-    void countBooks(){
-        System.out.println("Total books in library: " + books.size());
-    }
-
-    void findBookByTitle(String title){
-        if(books.isEmpty()){
+    void findBookByTitle(String title) {
+        if (books.isEmpty()) {
             System.out.println("No books in the library yet");
-        }
-        else {
-            for (Book book: books){
-                if (book.getTitle().equals(title)){
-                    System.out.println("The book <<" + title +">> found.");
+        } else {
+            for (Book book : books) {
+                if (book.getTitle().equals(title)) {
+                    System.out.println("The book <<" + title + ">> found.");
                     return;
                 }
 
@@ -178,11 +222,10 @@ class Library{
         }
     }
 
-    void removeBookById(int id){
-        if(books.isEmpty()){
+    void removeBookById(int id) {
+        if (books.isEmpty()) {
             System.out.println("No books in the library yet");
-        }
-        else {
+        } else {
             for (Book book : books) {
                 if (book.getId() == id) {
                     books.remove(book);
@@ -194,11 +237,10 @@ class Library{
         }
     }
 
-    void showExpensiveBooks(double price){
-        if (books.isEmpty()){
+    void showExpensiveBooks(double price) {
+        if (books.isEmpty()) {
             System.out.println("No books in the library yet");
-        }
-        else{
+        } else {
             System.out.println("Books price higher than " + price);
             int count = 0;
             for (Book book : books) {
@@ -207,15 +249,105 @@ class Library{
                     count++;
                 }
             }
-            if (count == 0){
+            if (count == 0) {
                 System.out.println("No books found with price higher than " + price);
             }
         }
     }
 
-    void showAddedBook(Book book){
+    void showAddedBook(Book book) {
         System.out.println("You added book: ");
         book.showInfo();
+    }
+
+    void findByAuthor(String author){
+        if(books.isEmpty()){
+            System.out.println("No books in the library yet");
+        }
+        else{
+            for (Book book: books){
+                if(book.getAuthor().equalsIgnoreCase(author)){
+                    book.showInfo();
+                    System.out.println("Book written by " +author+" has been found.");
+                    continue;
+                }
+            }
+            System.out.println("Book written by "+author+" wasn't found.");
+        }
+    }
+
+    void findById(int id){
+        if (books.isEmpty()) {
+            System.out.println("No books in the library yet");
+        } else {
+            for (Book book : books) {
+                if (book.getId() == id) {
+                    book.showInfo();
+                    System.out.println("The book with id " + id + " has been found.");
+                    return;
+                }
+            }
+            System.out.println("The book with id " + id + " wasn't found.");
+        }
+    }
+
+    int countBooks(){
+        return books.size();
+    }
+
+    double averagePrice(){
+        if (books.isEmpty()) return 0.0;
+        double average = 0.0;
+        double sum = 0.0;
+        for(Book book : books){
+            average= average +book.getPrice();
+        }
+        sum = average/books.size();
+        return sum;
+    }
+
+    double expensiveBook(){
+        if (books.isEmpty()) return 0.0;
+        double mostExpensive = books.getFirst().getPrice();
+        for(Book book : books){
+            if(book.getPrice()>mostExpensive){
+                mostExpensive = book.getPrice();
+            }
+        }
+        return mostExpensive;
+    }
+
+    double cheapBook(){
+        if (books.isEmpty()) return 0.0;
+        double mostCheap = books.getFirst().getPrice();
+        for (Book book : books){
+            if(book.getPrice()<mostCheap){
+                mostCheap = book.getPrice();
+            }
+        }
+        return mostCheap;
+    }
+    void showStatistic(){
+        if(books.isEmpty()){
+            System.out.println("No books in the library yet");
+        }
+        else{
+            System.out.println("==========LIBRARY STATISTIC==========\n"+
+                    "Total books in library: " + countBooks()+
+                    "\nAverage price: " + averagePrice() +
+                    "\nThe most expensive book: " + expensiveBook() +
+                    "\nThe cheapest book: " + cheapBook()+
+                    "\n=====================================");
+        }
+    }
+    void sortByPrice(){
+        if(books.isEmpty()){
+            System.out.println("No books in the library yet");
+        }
+        else{
+            books.sort((b1, b2) -> Double.compare(b1.getPrice(), b2.getPrice()));
+            System.out.println("Books were sorted. New list:\n");
+        }
     }
 }
 
@@ -225,10 +357,47 @@ class Menu{
                 +"\nHello! What you want to do?\n" +
                 "1 - Add book\n" +
                 "2 - Show books\n" +
-                "3 - Find book\n" +
+                "3 - Find book by ID\n" +
                 "4 - Remove book\n" +
-                "5 - Show expensive books\n" +
+                "5 - Find book by title\n" +
+                "6 - Show expensive books\n" +
+                "7 - Sort books\n" +
+                "8 - Show Statistic\n" +
+                "9 - Find book by author\n" +
                 "0 - Exit\n" +
                 "----------------------------------");
     }
 }
+
+
+enum MenuChoice{
+    ADD_BOOK(1),
+    SHOW_BOOKS(2),
+    FIND_BY_ID(3),
+    REMOVE_BOOK(4),
+    FIND_BY_TITLE(5),
+    SHOW_EXPENSIVE(6),
+    SORT_BOOKS(7),
+    SHOW_STATISTIC(8),
+    FIND_BY_AUTHOR(9),
+    EXIT(0);
+
+    private final int value;
+
+    MenuChoice(int value){
+        this.value = value;
+    }
+    public int getValue(){
+        return value;
+    }
+
+    public static MenuChoice fromInt(int choice){
+        for (MenuChoice command : MenuChoice.values()){
+            if(command.getValue() == choice){
+                return command;
+            }
+        }
+        return null;
+    }
+}
+
